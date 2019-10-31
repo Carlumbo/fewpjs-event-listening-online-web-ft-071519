e4279419a54900e5f81822bb2037411f99a19b7f
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-10-31 19:34:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const input = document.querySelector('input');
 
 function addingEventListener() {
   input.addEventListener('click', function(event) {
-    alert("I was clicked!"); 
+    alert("I was clicked!");
 } )
 }
